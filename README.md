508c728f8ac7df71046ff13edfd0ba1b0a74621c
aliounedia <dia.aliounes@gmail.com>
2014-06-20 11:07:02 +0200

    update

--- README.md
+++ README.md
@@ -2,3 +2,41 @@ pcci-apps
 =========
 
 apps run at PCCI
+
+
+* this app run on django Django-1.6.5 , The latest version when 
+I wrote it with python 27. It monitor The DMC (Maximal call 
+duration ) and DMT (Maximal call handling) to each outband call
+to alert timemout. 90 percent of code is into admin.py and models.py
+
+
+*installing app dmt_timeout 
+
+    **install django :
+      see https://www.djangoproject.com
+
+    **create app
+      manage.py startapp dmt_timeout
+
+    **install models.py for existing mysql pcci database
+    python manage.py inspectdb > dmt_timeout/models.py
+  
+    ** remove not using models into models.py
+     For me I use only Main , Statistiques
+
+    ** change some type field to take adventage of the powefull django 
+     ORM .For me only one change Type I done Here :
+
+    # change CharField to IntegerField  
+    main_tempscom = models.CharField(db_column='Main_TempsCom', max_length=20, blank=True) # Field name made lowercase.
+    to 
+    main_tempscom = models.IntegerField(db_column='Main_TempsCom', max_length=20, blank=True) # Field name made lowercase.
+  
+  
+ * Sync db 
+  manage.py syncdb 
+  
+ * Run server
+ manage.py runserver HOST:PORT
+
+ 
---
+++ pcciprojects/dmt_timeout/admin.py
@@ -0,0 +1,168 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from models import Main
+
+from datetime import date
+
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+class TimeOutListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('decade born')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'decade'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('Depassement', _('Depassement duree  traitement appel ')),
+            ('NoDepassement', _('pas de Depassement duree  traitement appel')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+
+        print 'query'
+        print self.value()
+            
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == 'Depassement':
+            return queryset.filter(main_tempscom__gt =480)
+        if self.value() == 'NoDepassement':
+           return queryset.filter(main_tempscom__lt=480)
+
+class FileListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('file filter title')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'file'
+
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+
+
+        print "The request "
+        if self.value():
+            return queryset.filter(main_fichier =self.value())
+        
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return Main.objects.filter(
+            main_dateappel__gt =date.today()).values_list(
+                "main_fichier", "main_fichier").distinct()
+
+
+        
+class UserListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('user filter title')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'user'
+
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+
+
+        print "The request "
+        if self.value():
+            return queryset.filter(main_userid =self.value())
+        
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return Main.objects.filter(
+            main_dateappel__gt =date.today()).values_list(
+                "main_userid", "main_userid").distinct()
+
+
+        
+class MainAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+        
+    def admin_main_dateappel(self):
+        return self.main_dateappel.hour
+    admin_main_dateappel.short_description ="Heure Appel"
+
+    def admin_main_fichier(self):
+        return self.main_fichier
+    admin_main_fichier.short_description ="Fichier"
+
+    def admin_main_tempscom(self):
+        return self.main_tempscom
+    admin_main_tempscom.short_description ="DMT"
+
+    def admin_timeout_count(self):
+        return self.timeout_count()
+    admin_timeout_count.short_description ="Nombre"
+
+    def admin_on_timeout(self):
+        if self.on_timeout()== True:
+            return '<img src= "/static/admin/img/icon_error.gif"/>'
+        return  '<img src= "/static/admin/img/icon_success.gif"/>' 
+
+    admin_on_timeout.short_description ="Statut"
+    admin_on_timeout.allow_tags        =True    
+    
+    #list_display_links = (None,)
+    def queryset(self, request):
+        return super(MainAdmin, self).queryset(request).filter(
+            main_dateappel__gt =date.today(),
+            main_tempscom__isnull=False).order_by("-main_dateappel")
+    
+    
+    search_fields = ['main_userid', 'main_fichier', 'on_timeout']
+    list_display = ('main_userid',admin_main_fichier, admin_main_dateappel,
+                    admin_main_tempscom, admin_timeout_count, admin_on_timeout)
+    # No link 
+    list_filter   = (TimeOutListFilter,UserListFilter,FileListFilter)
+
+  
+       
+admin.site.register(Main, MainAdmin)
+
